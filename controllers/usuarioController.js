32b719099d46531a38f95be0993afce708754505
Otto Szarata <szystems@hotmail.com>
2022-09-21 17:22:33 -0600

    formularios auth

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -2,17 +2,25 @@
 
 const formularioLogin = (req, res) => {
     res.render('auth/login', {
+        pagina: 'Iniciar sesion'
 
     })
 }
 
-const formularioLRegistro = (req, res) => {
+const formularioRegistro = (req, res) => {
     res.render('auth/registro', {
-        
+        pagina: 'Registro'
+    })
+}
+
+const formularioOlvidePassword = (req, res) => {
+    res.render('auth/olvide-password', {
+        pagina: 'Olvide mi password'
     })
 }
 
 export {
     formularioLogin,
-    formularioLRegistro
+    formularioRegistro,
+    formularioOlvidePassword
 }
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -2,13 +2,14 @@
 import express from "express";
 
 //importamos formularioLogin
-import { formularioLogin, formularioLRegistro } from "../controllers/usuarioController.js";
+import { formularioLogin, formularioRegistro ,formularioOlvidePassword} from "../controllers/usuarioController.js";
 
 //definimos router
 const router = express.Router();
 
 router.get('/login', formularioLogin);
-router.get('/registro', formularioLRegistro);
+router.get('/registro', formularioRegistro);
+router.get('/olvide-password', formularioOlvidePassword)
 
 //exportamos router
 export default router
